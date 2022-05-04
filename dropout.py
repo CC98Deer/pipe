b2de6edf57572e69337e7bd9befafecbb5fb5026
Lin Yekai <3170106097@zju.edu.cn>
2022-05-04 21:36:11 +0800

    dropout probality distribution

--- dropout.py
+++ dropout.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import numpy as np
 
+total = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
 # dropout函数的实现
 def dropout(x, level):
     if level < 0. or level >= 1: #level是概率值，必须在0~1之间
@@ -11,14 +12,24 @@ def dropout(x, level):
     # 硬币 正面的概率为p，n表示每个神经元试验的次数
     # 因为我们每个神经元只需要抛一次就可以了所以n=1，size参数是我们有多少个硬币。
     random_tensor = np.random.binomial(n=1, p=retain_prob, size=x.shape) #即将生成一个0、1分布的向量，0表示这个神经元被屏蔽，不工作了，也就是dropout了
-    print(random_tensor)
+    #print(random_tensor)
+    num = 0
+    for item in random_tensor:
+	    if item == 0 :
+		    num += 1
+    total[num] += 1
+		
 
     x *= random_tensor
-    print(x)
+    #print(x)
     x /= retain_prob
 
     return x
 
 #对dropout的测试，大家可以跑一下上面的函数，了解一个输入x向量，经过dropout的结果  
 x=np.asarray([1,2,3,4,5,6,7,8,9,10],dtype=np.float32)
-dropout(x,0.4)
+for i in range(10000):
+    dropout(x,0.4)
+for (key, value) in total.items():
+    total[key] /= 10000
+print(str(total))
